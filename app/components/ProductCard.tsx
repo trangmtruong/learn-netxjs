c87fa45bc8954f7991b051a9a48f6c893d11c7e0
Trang T <trangmtruong@yahoo.com>
2025-10-29 13:53:01 -0700

    practice csr

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('Button clicked')}>Add to Cart</button>
+    </div>
+  )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main>
         <h1>Hello World</h1>
         <Link href="/users">Go to Users Page</Link>
+        <ProductCard />
 
       </main>
     
